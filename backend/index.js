7dded5a562e5ec33e91b0843a4b898fbf029ab59
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-06-04 18:00:36 +0530

    feat: connect mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
 import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Welcome to the backend server of bookstore!</h1>");
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
