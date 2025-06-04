75f3a81e4c4babd54524b230e60d7f6911a2ab3c
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-06-04 18:38:07 +0530

    feat: implement create-book functionality

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,32 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 
-const app = express();
+import { Book } from "./models/bookModel.js";
+
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+const app = express();
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.status(200).send("<h1>Welcome to the backend server of bookstore!</h1>");
+});
+
+app.post("/books", async (req, res) => {
+  try {
+    const { title, author, publishYear } = req.body;
+
+    if (!title || !author || !publishYear) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const newBook = await Book.create({ title, author, publishYear });
+
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating book", error });
+  }
 });
 
 mongoose
