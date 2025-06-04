6d8fd20f7df623795af12a20946ab5bcc91712cb
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-06-04 16:07:46 +0530

    feat: add root route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Welcome to the backend server of bookstore!</h1>");
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
