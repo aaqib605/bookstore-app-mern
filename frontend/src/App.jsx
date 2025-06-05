d5062dd40c05e0e32fae3ecfd50aa78f5735c0e7
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-06-05 11:43:27 +0530

    chore: setup vite-react frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div className="bg-red-600 text-white text-center h-screen flex items-center justify-center flex-col gap-10">
+      <h1>Welcome to the React App</h1>
+      <p>This is a simple React application.</p>
+    </div>
+  );
+}
