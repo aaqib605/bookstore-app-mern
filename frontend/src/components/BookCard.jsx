82d949d05ebeea4d25a0554f3dfbdf862d44d402
aaqibjavaid <aaqibjavaid605@gmail.com>
2025-06-05 15:29:57 +0530

    feat: implement book details modal

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -7,7 +7,11 @@ import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineDelete } from "react-icons/md";
 
+import BookModal from "./BookModal";
+
 export default function BookCard({ book }) {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className="border-2 border-gray-500 rounded-lg px-4 py-2 m-4 relative hover:shadow-xl">
       <h4 className="my-2 text-gray-500">{book._id}</h4>
@@ -26,6 +30,7 @@ export default function BookCard({ book }) {
       <div className="flex justify-between items-center gap-x-2 mt-4 p-4">
         <BiShow
           className="text-3xl text-blue-800 hover:text-black cursor-pointer"
+          onClick={() => setShowModal(true)}
         />
         <Link to={`/books/details/${book._id}`}>
           <BsInfoCircle className="text-2xl text-green-800 hover:text-black" />
@@ -37,6 +42,10 @@ export default function BookCard({ book }) {
           <MdOutlineDelete className="text-2xl text-red-600 hover:text-black" />
         </Link>
       </div>
+
+      {showModal && (
+        <BookModal book={book} onClose={() => setShowModal(false)} />
+      )}
     </div>
   );
 }
